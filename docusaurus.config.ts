386de35ffc1b3d6a2c0f06859bc8dd99a7243d2c
David L.E <dl.eeee.nv@gmail.com>
2025-05-18 18:26:27 +0800

    sad

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
 
   // Set the production url of your site here
   // url: 'https://your-docusaurus-site.example.com',
-  url: `https://${organizationName}`,
+  url: `https://${organizationName}.github.io`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: `/${projectName}/`,
