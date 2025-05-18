d48f5e783f26853cce3128efac1202c2515fde41
David L.E <dl.eeee.nv@gmail.com>
2025-05-18 19:10:29 +0800

    udpate

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -54,15 +54,18 @@ const config: Config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
+          postsPerPage: 5,
+          blogSidebarTitle: 'Recent posts',
+          blogSidebarCount: 'ALL',
+          showLastUpdateTime: true,
           showReadingTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            `https://github.com/${organizationName}/${projectName}/tree/main/`,
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -89,7 +92,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Tuuto',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
